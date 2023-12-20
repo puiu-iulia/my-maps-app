08fe311c04cd9c81dd0ce956aabd24464c20e1a2
Iulia Puiu <iulia.puiu@hotmail.com>
2023-12-20 14:26:11 +0200

    Add zoom and focus map

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
-import { View, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
-import MapView from 'react-native-maps'
+import { View, StyleSheet, Button, TouchableOpacity } from 'react-native'
+import React, { useRef, useState } from 'react'
+import MapView, { Region } from 'react-native-maps'
+import { Ionicons } from '@expo/vector-icons'
 
 const INITIAL_REGION = {
     latitude: 37.78825,
@@ -12,6 +13,27 @@ const INITIAL_REGION = {
 const index = () => {
 
     const [region, setRegion] = useState(INITIAL_REGION)
+    const mapRef = useRef<MapView>(null)
+
+    const onRegionChange = (region: Region) => {
+        //setRegion(region)
+        console.log(region)
+    }
+
+    const focusMap = () => {
+        const NewYorkRegion = {
+            latitude: 40.7128,
+            longitude: -74.0060,
+            latitudeDelta: 0.0922, // Zoom level
+            longitudeDelta: 0.0421, // Zoom level
+        }
+
+        mapRef.current?.animateToRegion(NewYorkRegion)
+    }
+
+    const zoom = () => {
+        mapRef.current?.animateCamera({ zoom: 16 })
+    }
 
     return (
         <View style={styles.container}>
@@ -23,7 +45,16 @@ const index = () => {
                 showsUserLocation
                 showsMyLocationButton
                 initialRegion={region}
+                onRegionChange={onRegionChange}
             />
+            <View style={styles.btnContainer}>
+                <TouchableOpacity style={styles.btn} onPress={focusMap} >
+                    <Ionicons name='business' size={24} />
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.btn} onPress={zoom} >
+                    <Ionicons name='earth' size={24} />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -34,6 +65,17 @@ const styles = StyleSheet.create({
     },
     map: {
         flex: 1,
+    },
+    btnContainer: {
+        position: 'absolute',
+        bottom: 110,
+        right: 10,
+        gap: 10,
+    },
+    btn: {
+        backgroundColor: 'white',
+        borderRadius: 48,
+        padding: 16,
     }
 })
 
